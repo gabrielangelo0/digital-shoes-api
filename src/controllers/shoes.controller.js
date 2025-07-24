9206b6f710638517e1c34794648c0ec61523f870
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-07-24 09:26:54 -0300

    added crud

--- src/controllers/shoes.controller.js
+++ src/controllers/shoes.controller.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 
 const createShoes = async (req, res) => {
   const { name, brand, price, size, color, imageUrl, inStock } = req.body;
-  
+
   const shoes = await prisma.shoes.create({
     data: {
       name,
@@ -26,7 +26,66 @@ const getAllShoes = async (req, res) => {
   res.status(200).json(shoes)
 }
 
+const deleteShoes = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.shoes.delete({
+      where: { id }
+    })
+
+    res.status(204).send('Sapato deletado com sucesso!')
+  } catch (error) {
+    console.error('Error deleting shoe:', error)
+    return res.status(500).send('Erro ao deletar sapato.')
+  }
+}
+
+const updateShoes = async (req, res) => {
+  const { id } = req.params;
+
+  const { name, brand, price, size, color, imageUrl, inStock } = req.body;
+
+  try {
+    await prisma.shoes.update({
+      where: { id },
+      data: {
+        name,
+        brand,
+        price,
+        size,
+        color,
+        imageUrl,
+        inStock
+      }
+    })
+
+    res.status(200).send('Sapato atualizado com sucesso!')
+  } catch (error) {
+    console.error('Error updating shoe:', error)
+    return res.status(500).send('Erro ao atualizar sapato.')
+  }
+}
+
+const getShoes = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const shoes = await prisma.shoes.findUnique({
+      where: { id }
+    })
+
+    res.status(200).json(shoes)
+  } catch (error) {
+    console.error('Error fetching shoe:', error)
+    return res.status(500).send('Erro ao buscar sapato.')
+  }
+}
+
 module.exports = {
   createShoes,
-  getAllShoes
+  getAllShoes,
+  getShoes,
+  deleteShoes,
+  updateShoes
 }
--- src/routes/shoes.route.js
+++ src/routes/shoes.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createShoes, getAllShoes } = require('../controllers/shoes.controller');
+const { createShoes, getAllShoes, deleteShoes, updateShoes, getShoes } = require('../controllers/shoes.controller');
 
 const router = express.Router();
 
 router.post('/shoes', createShoes)
 router.get('/shoes', getAllShoes)
+router.delete('/shoes/:id', deleteShoes)
+router.put('/shoes/:id', updateShoes)
+router.get('/shoes/:id', getShoes)
 
 module.exports = router;
